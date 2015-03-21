a0a38bdafdcea1a58a369f8faaa97130428e2792
Chris Brown <chris@thebrown.net>
2015-03-21 20:39:08 +0000

    Zoom and pan

--- webgl.js
+++ webgl.js
@@ -1,16 +1,12 @@
 $(function () {
     var canvas = $('canvas')[0];
 
-    var gl;
-    try {
-        gl = canvas.getContext('experimental-webgl');
-        gl.viewportWidth = canvas.width;
-        gl.viewportHeight = canvas.height;
-    } catch (e) {
-        console.error('failed to initialize webgl');
-        throw e;
-    }
+    var gl = canvas.getContext('experimental-webgl');
 
+    /**
+     * @param type either gl.VERTEX_SHADER or gl.FRAGMENT_SHADER
+     * @param elementSelector a jQuery selector for an element which contains the shader source
+     */
     function createShader(type, elementSelector) {
         var shader = gl.createShader(type);
         gl.shaderSource(shader, $(elementSelector).text());
@@ -37,16 +33,24 @@ $(function () {
 
     gl.useProgram(program);
 
+    // this attribute is used to specify the position of each vertex
     vertexPositionAttribute = gl.getAttribLocation(program, 'vertexPosition');
     gl.enableVertexAttribArray(vertexPositionAttribute);
 
 /*    vertexColourAttribute = gl.getAttribLocation(program, 'vertexColour');
     gl.enableVertexAttribArray(vertexColourAttribute);
 */
+
+    // this unform is used to specify the viewport for the fractal
+    var viewportUniform = gl.getUniformLocation(program, 'viewport');
+
+    // two triangles which cover the whole screen
+    // they use normalized device coordinates, to save convering them in the vertex shader
     var vertexPositions = [
         -1.0, -1.0,
         -1.0,  1.0,
          1.0,  1.0,
+
          1.0,  1.0,
         -1.0, -1.0,
          1.0, -1.0
@@ -71,8 +75,48 @@ $(function () {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColours), gl.STATIC_DRAW);
     gl.vertexAttribPointer(vertexColourAttribute, 3, gl.FLOAT, false, 0, 0);
 */
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
+    // the screen is centred on these coordinates
+    var centreX = -0.5;
+    var centreY = 0;
+    // range of values that will fit on the screen
+    var scaleX = 3;
+    var scaleY = 3;
+
+    var lastMouseX, lastMouseY;
+    $(document).mousemove(function (event) {
+        if (lastMouseX || lastMouseY) {
+            var dx = event.pageX - lastMouseX;
+            var dy = event.pageY - lastMouseY;
+            // adjust the centre point when the mouse is moved
+            centreX += scaleX * dx * 0.01;
+            centreY += scaleY * dy * 0.01;
+        }
+        lastMouseX = event.pageX;
+        lastMouseY = event.pageY;
+    });
+
+    function drawFrame() {
+        // clear screen
+        gl.clearColor(0, 0, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // zoom in
+        scaleX *= 0.997;
+        scaleY *= 0.997;
+
+        // calculate top-left value
+        var x = centreX - scaleX / 2;
+        var y = centreY - scaleY / 2;
+        // set the viewport
+        gl.uniform4f(viewportUniform, x, y, scaleX, scaleY);
+
+        // draw the two triangles
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    }
+
+    (function animationFrame() {
+        requestAnimationFrame(animationFrame);
+        drawFrame();
+    })();
 });
