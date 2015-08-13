b864c50938d9eaef690a333a4d93b8a90372de0a
Chris Brown <chris@thebrown.net>
2015-08-13 20:23:24 +0100

    Pass starting triangle from JS

--- penrose.js
+++ penrose.js
@@ -1,5 +1,7 @@
 $(function () {
-    var RATE = 0.001;
+    var RATE = 0.0005;
+    var HALF_DART = 1; // sitting on wide base, t0 is bottom-left, t1 is bottom-right, t2 is top
+    var HALF_KITE = 2; // sitting on narrow base, t0 is bottom-left, t1 is bottom-right, t2 is top
 
     var canvas = $('canvas')[0];
     var aspect = canvas.width / canvas.height;
@@ -44,8 +46,12 @@ $(function () {
     var aspectUniform = gl.getUniformLocation(program, 'aspect');
     gl.uniform1f(aspectUniform, aspect);
 
-    // Time in radians (it loops)
     var timeUniform = gl.getUniformLocation(program, 'time');
+    var t0Uniform = gl.getUniformLocation(program, 'start_t0');
+    var t1Uniform = gl.getUniformLocation(program, 'start_t1');
+    var t2Uniform = gl.getUniformLocation(program, 'start_t2');
+    var triangleTypeUniform = gl.getUniformLocation(program, 'start_triangleType');
+    var signUniform = gl.getUniformLocation(program, 'start_sign');
 
     // two triangles which cover the whole screen
     // they use normalized device coordinates, to save convering them in the vertex shader
@@ -67,10 +73,15 @@ $(function () {
     var start = new Date().getTime();
 
     function drawFrame() {
+        // Starting half-kite that fills the screen.
+        gl.uniform1i(triangleTypeUniform, HALF_KITE);
+        gl.uniform2f(t0Uniform, 2.5, -2.5);
+        gl.uniform2f(t1Uniform, 0.954915021, 2.25528257925);
+        gl.uniform2f(t2Uniform, -7.5, -2.5);
+        gl.uniform1f(signUniform, 1);
+
         var time = (new Date().getTime() - start) * RATE;
-        while (time >= Math.PI * 2) {
-            time -= Math.PI * 2;
-        }
+        time = time - Math.floor(time);
         gl.uniform1f(timeUniform, time);
 
         // clear screen
